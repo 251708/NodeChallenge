92e70b168de7fd37a4ad6110a9c39d85e7e70d4f
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-14 23:32:02 +0530

    first commit

---
+++ First/index1.js
@@ -0,0 +1,17 @@
+//for coming out from the repl environment we can use .exit
+//for making a folder we can easily type mkdir after that that's folder name that we want to write
+//for making a file inside a folder we use type null>and after that file name
+//.exit is used for comeout from the repl
+/*
+Repl
+1: js expression 
+2:use variables 
+3:Multiline code 
+4:use(_) to get the last result
+5: we can use editor mode 
+*/
+console.log("welcome to my channel:");
+
+
+
+
